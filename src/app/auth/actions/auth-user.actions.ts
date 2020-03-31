7eed91d58c0c52678cf14e1ed567eec59ef35c04
Penrod <Christian.Penrod@McKesson.com>
2020-03-31 16:49:55 -0400

    Auth Module Challenge - Step One

--- src/app/auth/actions/auth-user.actions.ts
+++ src/app/auth/actions/auth-user.actions.ts
@@ -1,1 +1,5 @@
 import { createAction } from "@ngrx/store";
+
+export const enter = createAction('[Auth User] Enter');
+export const logIn = createAction('[Auth User] Login');
+export const logOut = createAction('[Auth User] Logout');
